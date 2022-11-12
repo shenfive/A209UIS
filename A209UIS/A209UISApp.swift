f832dd56101e75b8b2871338403eb8254ad8f29f
shenfive@gmail.com <Danny Shen>
2022-11-12 14:22:23 +0800

    Initial Commit

---
+++ A209UIS/A209UISApp.swift
@@ -0,0 +1,17 @@
+//
+//  A209UISApp.swift
+//  A209UIS
+//
+//  Created by 申潤五 on 2022/11/12.
+//
+
+import SwiftUI
+
+@main
+struct A209UISApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
